c089508a2dee7fbfb127bada017b6318cf92f6de
PriyaS0309 <109306451+PriyaS0309@users.noreply.github.com>
2022-07-25 16:42:54 +0530

    Add project files.

---
+++ Cunstructor_Overloading/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor_overloading
+{
+    class Program
+    {
+        public Program()
+        {
+            Console.WriteLine("It is a Default Constructor!!");
+        }
+        public Program(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+
+        public Program(string a, string b)
+        {
+            Console.WriteLine("This is third Constructor !!{0}  {1}", a, b);
+        }
+
+        static void Main(string[] args)
+        {
+            //Program p = new Program();
+           // Program p1 = new Program(20,30); 
+            Program p2 = new Program("Priya", "Shweta");
+
+            Console.ReadLine();
+
+        }
+    }
+}
+
---
+++ Parameterized_Constructor/Employees.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Parameterized_Constructor
+{
+    class Employees
+    {
+        int EmpId;
+        string EmpName;
+        int salary;
+
+        public Employees(int EmpId,string EmpName,int salary)
+        {
+            this.EmpId = EmpId;                                           //setting the values to the members present in the class
+            this.EmpName = EmpName;
+            this.salary = salary;
+        }
+        public int getId()                                                // to get the values from the variables i.e to display the result
+        {
+            return this.EmpId;
+        }
+        public string getName()                                                // to get the values i.e to display the result
+        {
+            return this.EmpName;
+        }
+        public int getSalary()                                                // to get the values i.e to display the result
+        {
+            return this.salary;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Employees priya= new Employees(1,"Priya",20000);                  //creating objects of constructor(u can create as many objects of construcor)
+            Employees shweta = new Employees(2, "Shweta",30000);
+
+           Console.WriteLine("Priya's info: {0} {1} {2}",priya.getId(),priya.getName(),priya.getSalary());                 //to display output
+           Console.WriteLine("Shweta's info: {0} {1} {2}", shweta.getId(), shweta.getName(), shweta.getSalary());
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
